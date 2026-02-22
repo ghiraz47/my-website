58ec332746d7b55b138efde9e18f72474e78d699
ghiraz47 <ghiath.razook@gmail.com>
2026-02-22 17:47:36 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("feedbackBtn").addEventListener("click", function () {
+  document.getElementById("message").textContent =
+    "Thank you! Your feedback has been received.";
+});
