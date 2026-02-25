e25efa316c2fc722c36aa2ea0c827c06e7d453d5
ghiraz47 <ghiath.razook@gmail.com>
2026-02-25 09:02:35 +0200

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("feedbackBtn").addEventListener("click", function () {
-  document.getElementById("message").textContent =
-    "Thank you! Your feedback has been received.";
-});
